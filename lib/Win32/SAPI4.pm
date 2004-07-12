34b5cd171905b5836b5f66c712e2fdd5e183a08e
Jouke Visser <jouke@cpan.org>
2004-07-12 19:06:27 +0000

    Import of JOUKE/Win32-SAPI4-0.04 from CPAN.
    gitpan-cpan-distribution: Win32-SAPI4
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         JOUKE/Win32-SAPI4-0.04.tar.gz
    gitpan-cpan-author:       JOUKE
    gitpan-cpan-maturity:     released

--- lib/Win32/SAPI4.pm
+++ lib/Win32/SAPI4.pm
@@ -1,67 +1,109 @@
 #!/usr/bin/perl
 package Win32::SAPI4;
+use Win32::OLE;
 use vars qw ($VERSION);
-$VERSION     = 0.03;
+$VERSION     = 0.04;
+
+BEGIN
+{
+    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
+    our %CLSID = ( VoiceText               => '{2398E32F-5C6E-11D1-8C65-0060081841DE}',
+                   VoiceCommand            => '{66523042-35FE-11D1-8C4D-0060081841DE}',
+                   VoiceDictation          => '{582C2191-4016-11D1-8C55-0060081841DE}',
+                   VoiceTelephony          => '{FC9E740F-6058-11D1-8C66-0060081841DE}',
+                   DirectSpeechRecognition => '{4E3D9D1F-0C63-11D1-8BFB-0060081841DE}',
+                   DirectSpeechSynthesis   => '{EEE78591-FE22-11D0-8BEF-0060081841DE}'                   
+                 );
+}
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = {};
+    (my $subclass = $proto) =~ s/.*:://;
+    $self->{_object} = Win32::OLE->new($CLSID{$subclass}) || return undef;
+    bless $self, $class;
+    return $self;
+}
+
+sub AUTOLOAD
+{
+    my $self = shift;
+    (my $auto = $AUTOLOAD) =~ s/.*:://;
+    my $params = '';
+    $params .= "$_," for grep{defined $_} @_;
+    my $call = '$self->{_object}->'."$auto($params)";
+    return eval($call);
+}
 
 package Win32::SAPI4::VoiceText;
-use Win32::OLE qw( EVENTS );
+use Win32::Locale;
+use Locale::Country;
+use Locale::Language;
+use base 'Win32::SAPI4';
 
-sub new
+sub GetInstalledLanguages
 {
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{2398E32F-5C6E-11D1-8C65-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
+    my $self = shift;
+    my %r;
+    for (my $i=1; $i <= $self->{_object}->CountEngines; $i++)
+    {
+        my ($t1, $t2) = split(/-/,Win32::Locale::get_language($self->{_object}->LanguageID($i)));
+        $r{code2language($t1)." (".code2country($t2).")"}++;
+    }
+    return keys %r;
 }
+
+sub GetInstalledVoices
+{
+    my $self = shift;
+    my $language = shift;
+    my @r;
+    for (my $i=1; $i <= $self->{_object}->CountEngines; $i++)
+    {
+        my ($t1, $t2) = split(/-/,Win32::Locale::get_language($self->{_object}->LanguageID($i)));
+        push @r, $self->{_object}->ModeName($i) if $language eq code2language($t1)." (".code2country($t2).")";
+    }
+    return @r;
+}
+
+sub Language2LanguageID
+{
+    my $self = shift;
+    my $language = shift;
+    for (my $i=1; $i <= $self->{_object}->CountEngines; $i++)
+    {
+        my ($t1, $t2) = split(/-/,Win32::Locale::get_language($self->{_object}->LanguageID($i)));
+        return $self->{_object}->LanguageID($i) if $language eq code2language($t1)." (".code2country($t2).")";
+    }
+}
+
+sub Voice2ModeID
+{
+    my $self = shift;
+    my $voice = shift;
+    for (my $i=1; $i <= $self->{_object}->CountEngines; $i++)
+    {
+        return $self->{_object}->ModeID($i) if $voice eq $self->{_object}->ModeName($i);
+    }
+}
+
 
 package Win32::SAPI4::VoiceCommand;
-use Win32::OLE qw( EVENTS );
-
-sub new
-{
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{66523042-35FE-11D1-8C4D-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
-}
+use base 'Win32::SAPI4';
 
 package Win32::SAPI4::VoiceDictation;
-use Win32::OLE qw( EVENTS );
-
-sub new
-{
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{582C2191-4016-11D1-8C55-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
-}
+use base 'Win32::SAPI4';
 
 package Win32::SAPI4::VoiceTelephony;
-use Win32::OLE qw( EVENTS );
-
-sub new
-{
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{FC9E740F-6058-11D1-8C66-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
-}
+use base 'Win32::SAPI4';
 
 package Win32::SAPI4::DirectSpeechRecognition;
-use Win32::OLE qw( EVENTS );
-
-sub new
-{
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{4E3D9D1F-0C63-11D1-8BFB-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
-}
+use base 'Win32::SAPI4';
 
 package Win32::SAPI4::DirectSpeechSynthesis;
-use Win32::OLE qw( EVENTS );
-
-sub new
-{
-    Win32::OLE->Initialize(Win32::OLE::COINIT_MULTITHREADED);
-    my $self = Win32::OLE->new('{EEE78591-FE22-11D0-8BEF-0060081841DE}') || die "Can't start VoiceText";
-    return $self;
-}
+use base 'Win32::SAPI4';
 
 =pod
 
@@ -86,7 +128,8 @@ This module is a simple interface to the Microsoft Speech API 4.0.
 It just offers 6 constructors to the different parts of this API.
 This documentation won't offer the complete documentation for it, just
 download the Microsoft Speech API 4.0 SDK and read the Visual Basic
-documentation. It's all there.
+documentation. It's all there, except for the few convenience methods I
+added to Win32::SAPI::VoiceText.
 
 =head1 PREREQUISITES
 
@@ -97,7 +140,35 @@ find the 4.0 version)
 =head1 USAGE
 
 See the Microsoft Speech API 4.0 Visual Basic documentation for 
-all methods, properties and events available.
+all methods, properties and events available, except the following:
+
+=head2 Win32::SAPI4::VoiceText
+
+=over 4
+
+=item GetInstalledLanguages
+
+This method returns a list of all installed languages with their
+countryname. It may look like ('Dutch (Netherlands)', 'Dutch (Belgium)',
+'English (United States)', 'Portuguese (Brazil)')
+
+=item GetInstalledVoices
+
+This method takes a language as returned by GetInstalledLanguages and returns
+a list of all installed voices with their language.
+It may look like ('Adult female (Dutch)', 'Microsoft Sam (US English)')
+
+=item Language2LanguageID
+
+This method takes a language as returned by GetInstalledLanguages and
+returns the corresponding LanguageID that VoiceText knows.
+
+=item Voice2ModeID
+
+This method takes a voice as returned by GetInstalledVoices and
+returns the corresponding ModeID that VoiceText knows.
+
+=back
 
 =head1 SUPPORT
 
@@ -112,7 +183,7 @@ You can email the author for support on this module.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2003 Jouke Visser. All rights reserved.
+Copyright (c) 2003-2004 Jouke Visser. All rights reserved.
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
